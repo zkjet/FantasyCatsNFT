bd4eb91f0b9087505145d522934ba632d239cbcd
zkjet <zkjet@mailfence.com>
2023-04-25 13:30:29 -0600

    added hardhat configuration for base l2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+  },
+  networks: {
+    // for testnet
+    'base-goerli': {
+      url: 'https://goerli.base.org',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+    // for local dev environment
+    'base-local': {
+      url: 'http://localhost:8545',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+  },
+  defaultNetwork: 'hardhat',
 };
 
 export default config;
