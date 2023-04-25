91390a4a1dc0dd601094c8efcf925c66635ee33d
zkjet <zkjet@mailfence.com>
2023-04-25 13:38:37 -0600

    updated NFT contract with correct import

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppeling/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721 {
